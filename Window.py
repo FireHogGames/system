303467654608c84b280b43665077462f03b081e8
hidde loman <hidde01@live.nl>
2018-12-14 16:41:11 +0100

    Made custom window. Uploading to pc

--- Window.py
+++ Window.py
@@ -4,17 +4,10 @@ class WinAPI(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        # create a prompt, an input box, an output label,
-        # and a button to do the computation
-        self.prompt = tk.Label(self, text="Enter a number:", anchor="w")
-        self.entry = tk.Entry(self)
-        self.submit = tk.Button(self, text="Submit")
-        self.output = tk.Label(self, text="")
+        self.prompt = tk.Label(self, text="TestWindow", anchor="w")#prompt
+        self.submit = tk.Button(self, text="OK", command="")
 
-        # lay the widgets out on the screen. 
         self.prompt.pack(side="top", fill="x")
-        self.entry.pack(side="top", fill="x", padx=20)
-        self.output.pack(side="top", fill="x", expand=True)
         self.submit.pack(side="right")
 
 
@@ -22,3 +15,6 @@ class WinAPI(tk.Frame):
         root = tk.Tk()
         WinAPI(root).pack(fill="both", expand=True)
         root.mainloop()
+
+    def CloseWindow():
+        this.close()
