6573bf427e77dda588400e634470a75124d2ffd0
hidde loman <hidde01@live.nl>
2018-12-15 18:41:27 +0100

    Updated software

--- Upload_Changes.bat
+++ Upload_Changes.bat
@@ -1,22 +1,28 @@
 @echo off
 
+echo ------System-----
 echo adding files
 git add System_Core.py
 git add LICENSE
 git add .gitignore
 git add Upload_Changes.bat
 git add Run.bat
+echo -----------------
 
 pause
 
+echo ------System-----
 echo commiting to master
 git commit -m "Updated software"
 echo Finished committing
+echo -----------------
 
 pause
 
+echo ------System-----
 echo pushing to origin
 git push origin
 
 echo Upload finished
+echo -----------------
 pause
