4c252cebf1ac238f37f646991c3e3c0812f8f29b
hidde loman <hidde01@live.nl>
2018-12-16 14:16:48 +0100

    Updated software

--- Upload_Changes.bat
+++ Upload_Changes.bat
@@ -3,10 +3,15 @@
 echo ------System-----
 echo adding files
 git add System_Core.py
+echo Updated System_Core.py
 git add LICENSE
+echo Updated LICENSE
 git add .gitignore
+echo Updated .gitignore
 git add Upload_Changes.bat
+echo updated Upload_Changes.bat
 git add Run.bat
+echo updated Run.bat
 echo -----------------
 
 pause
@@ -22,7 +27,7 @@ pause
 echo ------System-----
 echo pushing to origin
 git push origin
-
 echo Upload finished
 echo -----------------
+
 pause
